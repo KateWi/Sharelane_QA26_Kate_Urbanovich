58df3a0838d4b4a56ea8a5b0d6c81b8a74d3f2e1
KateWi <katya539063@yandex.ru>
2024-05-20 23:30:30 +0300

    Assert fix

--- src/test/java/ShareLaneTests.java
+++ src/test/java/ShareLaneTests.java
@@ -1,46 +1,54 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
 
 public class ShareLaneTests {
-    @Test
-    public void signUpPositiveTest() {
-        RemoteWebDriver driver = new ChromeDriver();
+    private WebDriver driver;
+
+    @BeforeMethod(alwaysRun = true)
+    public void setUp() {
+        this.driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void tearDown() {
+        this.driver.quit();
+    }
+
+    @Test
+    public void signUpPositiveTest() {
         driver.navigate().to("https://sharelane.com/cgi-bin/register.py?");
         WebElement zipCodeInput = driver.findElement(By.name("zip_code"));
         zipCodeInput.sendKeys("12345");
         driver.findElement(By.cssSelector("input[value=Continue]")).click();
         boolean isFirstNameDisplayed = driver.findElement(By.name("first_name")).isDisplayed();
-        Assert.assertTrue(isFirstNameDisplayed);
-        driver.quit();
+        Assert.assertTrue(isFirstNameDisplayed, "firstNameInput is displayed");
+
 
     }
 
     @Test
     public void signUpNegativeTest() {
-        RemoteWebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         driver.navigate().to("https://sharelane.com/cgi-bin/register.py?");
         WebElement zipCodeInput = driver.findElement(By.name("zip_code"));
         zipCodeInput.sendKeys("123");
         driver.findElement(By.cssSelector("input[value=Continue]")).click();
-        Assert.assertTrue(driver.findElement(By.cssSelector("span[class=error_message]")).isDisplayed());
-        driver.quit();
+        Assert.assertTrue(driver.findElement(By.cssSelector("span[class=error_message]")).isDisplayed(), "firstNameInput isn't displayed");
+        Assert.assertEquals(driver.findElement(By.cssSelector("span[class=error_message]")).getText(), "Oops, error on page. ZIP code should have 5 digits");
+
     }
 
     @Test
     public void RegistrationFormPositiveTest() {
-        RemoteWebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         driver.navigate().to("https://sharelane.com/cgi-bin/register.py?");
         WebElement zipCodeInput = driver.findElement(By.name("zip_code"));
         zipCodeInput.sendKeys("12345");
@@ -54,15 +62,12 @@ public class ShareLaneTests {
         WebElement passwordConfirmInput = driver.findElement(By.name("password2"));
         passwordConfirmInput.sendKeys("12345qW");
         driver.findElement(By.cssSelector("input[value=Register]")).click();
-        Assert.assertTrue(driver.findElement(By.cssSelector("span[class=confirmation_message]")).isDisplayed());
-        driver.quit();
+        Assert.assertTrue(driver.findElement(By.cssSelector("span[class=confirmation_message]")).isDisplayed(), "account created");
+        Assert.assertEquals(driver.findElement(By.cssSelector("span[class=confirmation_message]")).getText(), "Account is created!");
     }
 
     @Test
     public void RegistrationFormNegativeTest() {
-        RemoteWebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         driver.navigate().to("https://sharelane.com/cgi-bin/register.py?");
         WebElement zipCodeInput = driver.findElement(By.name("zip_code"));
         zipCodeInput.sendKeys("12345");
@@ -70,7 +75,7 @@ public class ShareLaneTests {
         WebElement firsNameInput = driver.findElement(By.name("first_name"));
         firsNameInput.sendKeys("4");
         driver.findElement(By.cssSelector("input[value=Register]")).click();
-        Assert.assertTrue(driver.findElement(By.cssSelector("span[class=error_message]")).isDisplayed());
-        driver.quit();
+        Assert.assertTrue(driver.findElement(By.cssSelector("span[class=error_message]")).isDisplayed(), "RegistrationForm invalid");
+        Assert.assertEquals(driver.findElement(By.cssSelector("span[class=error_message]")).getText(), "Oops, error on page. Some of your fields have invalid data or email was previously used");
     }
 }
